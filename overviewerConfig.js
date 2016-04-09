b55ae2c023796e2611062427ed897e6996c9a5e8
disk59 <phantasia.59.dai@gmail.com>
2016-04-09 17:58:52 +0900

    Map Generate

---
+++ overviewerConfig.js
@@ -0,0 +1,109 @@
+var overviewerConfig = {
+    "worlds": [
+        "My World"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                156, 
+                64, 
+                256
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1460191901, 
+            "name": "A regular render", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "My World", 
+            "maxZoom": 6, 
+            "path": "render1", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                156, 
+                64, 
+                256
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1460191901, 
+            "name": "Upper-right north direction", 
+            "poititle": "Markers", 
+            "north_direction": 1, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "My World", 
+            "maxZoom": 6, 
+            "path": "render2", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                156, 
+                64, 
+                256
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1460191901, 
+            "name": "Nighttime", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "My World", 
+            "maxZoom": 6, 
+            "path": "render3", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1460192321"
+    }
+};
