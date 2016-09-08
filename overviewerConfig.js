e61d1050117c649e1f30a594a0afeb76f87a7ec3
disk59 <phantasia.59.dai@gmail.com>
2016-09-09 03:11:14 +0900

    Map Generate

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 256
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1473271230, 
+            "last_rendertime": 1473357632, 
             "name": "A regular render", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -32,7 +32,7 @@ var overviewerConfig = {
                 256
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1473271230, 
+            "last_rendertime": 1473357632, 
             "name": "Upper-right north direction", 
             "poititle": "Markers", 
             "north_direction": 1, 
@@ -54,7 +54,7 @@ var overviewerConfig = {
                 256
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1473271230, 
+            "last_rendertime": 1473357632, 
             "name": "Nighttime", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -104,6 +104,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1473271790"
+        "cacheTag": "1473358163"
     }
 };
